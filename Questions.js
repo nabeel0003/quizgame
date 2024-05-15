1eac14187260df91b42b1ad262cc3bd4f94b9d89
NABEEL ARSHAD <nabeel@devblends.com>
2024-05-15 13:58:36 +0500

    code

---
+++ Questions.js
@@ -0,0 +1,47 @@
+export const questionData = [
+    {
+    question : 'who is the prime minister',
+    Options :[
+        'imran khan',
+        'shehbaz sharif',
+        'nawaz sharif',
+        'asif ali zardari'
+    ],
+
+    correct:1,
+    marked: -1,
+},
+
+{
+    question : 'who is the president of pakistan',
+    Options :[
+        'imran khan',
+        'shehbaz sharif',
+        'nawaz sharif',
+        'asif ali zardari'
+    ],
+
+    correct:2,
+    marked: -1,
+
+    
+},
+{
+    question : 'who is the chief justice of pakistan',
+    Options :[
+        'imran khan',
+        'shehbaz sharif',
+        'qazi issa',
+        'asif ali zardari'
+    ],
+
+    correct:3,
+    marked: -1,
+
+    
+},
+
+
+
+
+]
